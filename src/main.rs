8c886a2881faf5afd6a7bc0b9cc8e16e9c2790b0
Gastón Tarasconi <gaston.tarasconi@gmail.com>
2024-02-04 23:02:21 -0300

    feature, change print error from standard output to standard error

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,12 @@ fn main() {
     let ignore_case = env::var("IGNORE_CASE").is_ok();
 
     let config = Config::build(&args, ignore_case).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
