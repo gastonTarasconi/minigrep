6fd1883a63565d97236a856b754d69371e7c367e
Gastón Tarasconi <gaston.tarasconi@gmail.com>
2024-02-09 20:49:51 -0300

    improve code to use Iterators instead of Vectors

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,9 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
     let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-    let config = Config::build(&args, ignore_case).unwrap_or_else(|err| {
+    let config = Config::build(env::args(), ignore_case).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
